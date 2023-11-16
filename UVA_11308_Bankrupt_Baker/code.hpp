24115c181d9378b181b1e437245e93c72aa20668
ShinjiMC <nayapbrau@gmail.com>
2023-11-15 23:48:49 -0500

    Adding Bankrupt Baker

--- UVA_11308_Bankrupt_Baker/code.hpp
+++ UVA_11308_Bankrupt_Baker/code.hpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <unordered_map>
 #include <set>
@@ -13,30 +12,34 @@ int code()
     {
         std::string name;
         std::getline(std::cin >> std::ws, name);
-        std::transform(name.begin(), name.end(), name.begin(), ::toupper);
+        for (char &c : name)
+        {
+            c = std::toupper(c);
+        }
         long m, n, b;
         std::cin >> m >> n >> b;
         std::cin.ignore();
-        std::string ingredient;
-        long price;
         std::unordered_map<std::string, long> objects;
         while (m--)
         {
+            std::string ingredient;
+            long price;
             std::cin >> ingredient >> price;
             objects[ingredient] = price;
         }
-        std::string recipe, product;
-        long cantIngredients, cant;
         std::set<std::pair<long, std::string>> recipes;
-        std::cin.ignore();
         while (n--)
         {
             long priceT = 0;
+            std::string recipe;
             std::getline(std::cin >> std::ws, recipe);
+            long cantIngredients;
             std::cin >> cantIngredients;
             std::cin.ignore();
             while (cantIngredients--)
             {
+                std::string product;
+                long cant;
                 std::cin >> product >> cant;
                 auto it = objects.find(product);
                 if (it != objects.end())
@@ -46,21 +49,21 @@ int code()
             }
             recipes.insert({priceT, recipe});
         }
-        std::cout << name << std::endl;
+        std::cout << name << "\n";
         bool can = true;
         for (const auto &i : recipes)
         {
             if (b >= i.first)
             {
-                std::cout << i.second << std::endl;
+                std::cout << i.second << "\n";
                 can = false;
             }
         }
         if (can)
         {
-            std::cout << "Too expensive!" << std::endl;
+            std::cout << "Too expensive!\n";
         }
-        std::cout << std::endl;
+        std::cout << "\n";
     }
     return 0;
 }
